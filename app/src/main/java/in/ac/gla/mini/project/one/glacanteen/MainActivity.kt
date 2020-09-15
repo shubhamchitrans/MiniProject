a2382ca18d47097d94e4ed55d438060ba3b95738
dbc2201 <divyanshb22@gmail.com>
2020-09-15 14:04:30 +0530

    initial commit

---
+++ app/src/main/java/in/ac/gla/mini/project/one/glacanteen/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.ac.gla.mini.project.one.glacanteen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
